4b4447f07894727599925aa5ced91d786923de18
phamanhtuan-coder <0306221391@caothang.edu.vn>
2024-12-03 01:11:08 +0700

    Merge nhánh OTP to sang

--- app/src/main/java/com/example/ungdungquanlyvadieukhieniot_homeconnect/MainActivity.kt
+++ app/src/main/java/com/example/ungdungquanlyvadieukhieniot_homeconnect/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.ungdungquanlyvadieukhieniot_homeconnect.ui.screen.otp.OTPVerificationScreen
 import com.example.ungdungquanlyvadieukhieniot_homeconnect.ui.theme.UngDungQuanLyVaDieuKhienIoT_HomeConnectTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,8 +20,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             UngDungQuanLyVaDieuKhienIoT_HomeConnectTheme {
-
-
+                OTPVerificationScreen()
             }
         }
     }
