672e286b7d0108148e69a575f259b0ec70ea763c
phamanhtuan-coder <0306221391@caothang.edu.vn>
2024-12-05 21:48:57 +0700

    Add 1 số Screens route

--- app/src/main/java/com/example/ungdungquanlyvadieukhieniot_homeconnect/ui/navigation/Screens.kt
+++ app/src/main/java/com/example/ungdungquanlyvadieukhieniot_homeconnect/ui/navigation/Screens.kt
@@ -3,5 +3,19 @@ package com.example.ungdungquanlyvadieukhieniot_homeconnect.ui.navigation
 sealed class Screens(
     route: String
 ){
+    object Home: Screens("home")
+    object Login: Screens("login")
+    object Register: Screens("register")
+    object RecoverPassword: Screens("recover_password")
+    object UpdatePassword: Screens("update_password")
+    object NewPassword: Screens("new_password")
+    object OTP: Screens("otp")
+    object PasswordAuth: Screens("password_auth")
+    object Profile: Screens("profile")
+    object UpdateProfile: Screens("update_profile")
+    object UpdateDevice: Screens("update_device")
+    object AddDevice: Screens("add_device")
+    object Device: Screens("device")
+    object Setting: Screens("setting")
 
 }
