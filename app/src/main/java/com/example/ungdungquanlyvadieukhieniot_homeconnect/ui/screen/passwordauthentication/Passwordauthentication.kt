b045f1337d66e1dbe36f5097bca4eaf986f33872
mtclote <nhanbr12345@gmail.com>
2024-12-05 18:25:15 +0700

    giao dien xac thuc mat khau

---
+++ app/src/main/java/com/example/ungdungquanlyvadieukhieniot_homeconnect/ui/screen/passwordauthentication/Passwordauthentication.kt
@@ -0,0 +1,118 @@
+package com.example.ungdungquanlyvadieukhieniot_homeconnect.ui.screen.passwordauthentication
+
+import android.content.res.Configuration
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Lock
+import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+
+/** Giao diện màn hình mật khẩu mới (NewPassword Screen)
+ * -----------------------------------------
+ * Người viết: Phạm Xuân Nhân
+ * Ngày viết: 03/12/2024
+ * Lần cập nhật cuối: 04/12/2024
+ * -----------------------------------------
+ * Input: Không có
+ *
+ * Output: Column chứa các thành phần giao diện của màn hình mật khẩu mới
+ */
+
+@Preview(showBackground = true)
+@Composable
+fun UpdatePassWord() {
+    val configuration = LocalConfiguration.current
+    val screenHeightDp = configuration.screenHeightDp.dp
+    val screenWidthDp = configuration.screenWidthDp.dp
+
+    // Xác định xem nếu chúng ta đang ở chế độ ngang
+    val isLandscape = configuration.orientation == Configuration.ORIENTATION_LANDSCAPE
+
+    //  kích thước phản hồi
+    val horizontalPadding = when {
+        screenWidthDp < 360.dp -> 8.dp
+        screenWidthDp < 600.dp -> 16.dp
+        else -> 32.dp
+    }
+
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White),
+
+        )
+    {
+        Box(modifier = Modifier.align(Alignment.Center)) {
+            Column(
+                modifier = Modifier
+                    .padding(horizontal = horizontalPadding)
+                    .verticalScroll(rememberScrollState())
+                    .heightIn(max = 800.dp)
+                    .widthIn(max = 600.dp),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = if (isLandscape) Arrangement.Center else Arrangement.SpaceBetween
+            ) {
+                Text(
+                    text = "Xác Thực Mật Khẩu",
+                    fontSize = 20.sp,
+                    fontWeight = FontWeight.Bold,
+                    color = Color.Black
+                )
+                Spacer(modifier = Modifier.height(8.dp))
+                Text(
+                    text = "hãy nhập email để chúng tôi gửi mã xác thực.",
+                    fontSize = 14.sp,
+                    color = Color.Gray,
+                    textAlign = TextAlign.Center
+                )
+                Spacer(modifier = Modifier.height(24.dp))
+                //EMAIL
+                OutlinedTextField(
+                    value = "",
+                    onValueChange = {},
+                    label = { Text("EMAIL") },
+                    leadingIcon = { Icon(Icons.Filled.Lock, contentDescription = "Lock Icon") },
+                    visualTransformation = PasswordVisualTransformation(),
+                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+                    modifier = Modifier.fillMaxWidth(),
+                    singleLine = true
+                )
+                Spacer(modifier = Modifier.height(16.dp))
+
+                // Nút gửi xác thực
+                Button(
+                    onClick = { },
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    Text("Gửi xác thực")
+                }
+                Spacer(modifier = Modifier.height(16.dp))
+
+                TextButton(onClick = { }) {
+                    Text("⬅ Quay lại")
+                }
+            }
+        }
+    }
+}
+
