2584278fb6bba3864693162c75411b3385d74069
mtclote <nhanbr12345@gmail.com>
2024-12-05 19:49:19 +0700

    giao dien cap nhat thong tin ca nhan

--- app/src/main/java/com/example/ungdungquanlyvadieukhieniot_homeconnect/MainActivity.kt
+++ app/src/main/java/com/example/ungdungquanlyvadieukhieniot_homeconnect/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import com.example.ungdungquanlyvadieukhieniot_homeconnect.ui.screen.editprofile.EditProfileScreen
 import com.example.ungdungquanlyvadieukhieniot_homeconnect.ui.screen.login.LoginScreen
 import com.example.ungdungquanlyvadieukhieniot_homeconnect.ui.screen.newpass.NewPasswordScreen
 import com.example.ungdungquanlyvadieukhieniot_homeconnect.ui.screen.signup.SignupScreen
@@ -16,11 +17,11 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             UngDungQuanLyVaDieuKhienIoT_HomeConnectTheme {
-                NewPasswordScreen()
+                //NewPasswordScreen()
                 //SignupScreen()
                //LoginScreen()
                // HomeScreen()
-
+                EditProfileScreen()
 
             }
         }
--- app/src/main/java/com/example/ungdungquanlyvadieukhieniot_homeconnect/ui/screen/updatepassword/UpdatePassWord.kt
+++ app/src/main/java/com/example/ungdungquanlyvadieukhieniot_homeconnect/ui/screen/updatepassword/UpdatePassWord.kt
@@ -2,7 +2,18 @@ package com.example.ungdungquanlyvadieukhieniot_homeconnect.ui.screen.updatepass
 
 import android.content.res.Configuration
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
@@ -13,18 +24,18 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalConfiguration
-import androidx.compose.ui.text.input.PasswordVisualTransformation
-import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 /** Giao diện màn hình Cập nhật Mật Khẩu (Update PassWord Screen)
  * -----------------------------------------
@@ -59,7 +70,7 @@ fun UpdatePassWord() {
         screenHeightDp < 800.dp -> 12.dp
         else -> 16.dp
     }
-    var bien=false
+    var bien = false
 
     Box(
         modifier = Modifier
@@ -140,7 +151,7 @@ fun UpdatePassWord() {
                 ) {
                     Spacer(modifier = Modifier.width(4.dp))
                     Text(
-                        text =if (bien) "✔ Phải có ít nhất 8 ký tự" else "✘ Phải có ít nhất 8 ký tự",
+                        text = if (bien) "✔ Phải có ít nhất 8 ký tự" else "✘ Phải có ít nhất 8 ký tự",
                         color = if (bien) Color.Green else Color.Red,
                         fontSize = 12.sp
                     )
@@ -154,7 +165,7 @@ fun UpdatePassWord() {
                 ) {
                     Spacer(modifier = Modifier.width(4.dp))
                     Text(
-                        text = if (bien) "✔ Phải chứa một ký tự đặc biệt" else "✘ Phải chứa một ký tự đặc biệt" ,
+                        text = if (bien) "✔ Phải chứa một ký tự đặc biệt" else "✘ Phải chứa một ký tự đặc biệt",
                         color = if (bien) Color.Green else Color.Red,
                         fontSize = 12.sp
                     )
