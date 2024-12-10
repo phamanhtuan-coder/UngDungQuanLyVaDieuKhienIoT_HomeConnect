2fff490bb2cfd989082be699a4638f84ade7e60c
Nguyễn Thanh Sang <nguyenthanhsang92643@gmail.com>
2024-12-10 23:01:27 +0700

    Hoàn thành giao diện ListNotificationsScreen.kt

--- app/src/main/java/com/example/ungdungquanlyvadieukhieniot_homeconnect/ui/screen/listnotifications/ListNotificationsScreen.kt
+++ app/src/main/java/com/example/ungdungquanlyvadieukhieniot_homeconnect/ui/screen/listnotifications/ListNotificationsScreen.kt
@@ -5,14 +5,18 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -26,7 +30,14 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material3.FabPosition
 import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -34,7 +45,23 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.zIndex
+import com.example.ungdungquanlyvadieukhieniot_homeconnect.ui.component.Header
+import com.example.ungdungquanlyvadieukhieniot_homeconnect.ui.component.MenuBottom
+import com.example.ungdungquanlyvadieukhieniot_homeconnect.ui.component.NutHome
 
+/** Giao diện màn hình Thông Báo (ListNotificationsScreen)
+ * -----------------------------------------
+ * Người viết: Nguyễn Thanh Sang
+ * Ngày viết: 10/12/2024
+ * Lần cập nhật cuối: 10/12/2024
+ * -----------------------------------------
+ * Input:
+ *
+ * Output: Scaffold
+ *
+ * ---------------------------------------
+ */
 // Kích thước và padding
 private object NotificationStyle {
     val cardPadding = 8.dp
@@ -56,7 +83,6 @@ private object NotificationColors {
     val readIconColor = Color.Green
     val notificationIconColor = Color.White
     val unreadIconBackground = Color(0xFFE74C3C)
-    val statusBackgroundColor = Color(0xFFFFEDED)
     val grayTextColor = Color.Gray
 }
 
@@ -82,20 +108,20 @@ fun EmptyNotificationScreen() {
     ) {
         Icon(
             imageVector = Icons.Default.Notifications,
-            contentDescription = "No Notifications",
+            contentDescription = "Không có thông báo",
             tint = NotificationColors.grayTextColor,
             modifier = Modifier.size(NotificationStyle.noNoticeIconSize)
         )
         Spacer(modifier = Modifier.height(NotificationStyle.textPadding))
         Text(
-            text = "No Notices Right Now!",
+            text = "Không có thông báo ngay bây giờ!",
             color = NotificationColors.grayTextColor,
             fontSize = NotificationStyle.titleFontSize,
             fontWeight = FontWeight.Bold
         )
         Spacer(modifier = Modifier.height(NotificationStyle.textPadding))
         Text(
-            text = "You're up-to-date!",
+            text = "Bạn đã cập nhật!",
             color = NotificationColors.grayTextColor,
             fontSize = NotificationStyle.descriptionFontSize
         )
@@ -104,7 +130,7 @@ fun EmptyNotificationScreen() {
             onClick = { /* Navigate */ },
             modifier = Modifier.height(NotificationStyle.buttonHeight)
         ) {
-            Text("View Dashboard")
+            Text("Xem trang tổng quan")
         }
     }
 }
@@ -112,7 +138,7 @@ fun EmptyNotificationScreen() {
 // Danh sách các thông báo
 @Composable
 fun NotificationList(notifications: List<Notification>) {
-    LazyColumn(modifier = Modifier.fillMaxSize()) {
+    LazyColumn(modifier = Modifier.fillMaxSize(), verticalArrangement = Arrangement.Top, horizontalAlignment = Alignment.CenterHorizontally) {
         // Kiểm tra và truyền đúng danh sách
         items(notifications) { notification ->
             NotificationCard(notification) // Hiển thị từng Card
@@ -120,13 +146,12 @@ fun NotificationList(notifications: List<Notification>) {
     }
 }
 
-
 // Card cho từng thông báo
 @Composable
 fun NotificationCard(notification: Notification) {
     Card(
         modifier = Modifier
-            .fillMaxWidth()
+            .width(500.dp)
             .padding(NotificationStyle.cardPadding)
             .clickable { /* Handle click */ },
         elevation = CardDefaults.cardElevation(NotificationStyle.cardElevation),
@@ -190,13 +215,105 @@ data class Notification(
 // Demo màn hình chính
 @Preview
 @Composable
-fun NotificationScreen() {
-    val notifications = listOf(
+fun ListNotificationScreen() {
+    val notifications = mutableListOf<Notification>(
+        Notification("New Feature Alert!", "We've introduced new features.", false),
+        Notification("System Update", "Your app has been updated.", true),
+        Notification("Reminder", "Your meeting is scheduled at 3 PM.", false),
+        Notification("New Feature Alert!", "We've introduced new features.", false),
+        Notification("System Update", "Your app has been updated.", true),
+        Notification("Reminder", "Your meeting is scheduled at 3 PM.", false),
         Notification("New Feature Alert!", "We've introduced new features.", false),
         Notification("System Update", "Your app has been updated.", true),
         Notification("Reminder", "Your meeting is scheduled at 3 PM.", false)
     )
 
-    NotificationList(notifications)
+    var showDialog by remember { mutableStateOf(false) }
+
+    return Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        containerColor = Color.LightGray,
+        topBar = { Header() },
+        bottomBar = { MenuBottom() },
+        floatingActionButton = { NutHome() },
+        floatingActionButtonPosition = FabPosition.Center,
+        content = { innerPadding ->
+            Column(
+                modifier = Modifier
+                    .padding(innerPadding)
+                    .fillMaxSize(), // Chiếm toàn bộ kích thước của màn hình
+//                    .padding(bottom = layoutConfig.outerPadding) // Padding linh hoạt
+                verticalArrangement = Arrangement.Top, // Sắp xếp các item theo chiều dọc, bắt đầu từ trên xuống.
+                horizontalAlignment = Alignment.CenterHorizontally // Căn chỉnh các item theo chiều ngang vào giữa.
+            ) {
+                // Box lớn chứa phần tiêu đề và các thành phần bên trong
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .wrapContentHeight() // Chiều cao vừa đủ với nội dung bên trong
+                        .background(color = Color.LightGray)
+                ) {
+                    // Cột chứa các phần tử con
+                    Column {
+                        // Hộp màu xanh dương bo tròn góc dưới bên trái
+                        Box(
+                            modifier = Modifier
+                                .fillMaxWidth() // Chiếm toàn bộ chiều rộng
+                                .wrapContentHeight() // Chiều cao vừa đủ với nội dung
+                                .background(
+                                    color = Color.Blue,
+                                    shape = RoundedCornerShape(bottomStart = 40.dp)
+                                )
+                        ) {
+                            // Cột chứa văn bản tiêu đề và các TextField
+                            Column(
+                                modifier = Modifier
+                                    .fillMaxWidth(), // Chiếm toàn bộ chiều rộng
+                                horizontalAlignment = Alignment.CenterHorizontally // Căn giữa các phần tử con theo chiều ngang
+                            ) {
+                                Spacer(modifier = Modifier.heightIn(8.dp))
+                                // Văn bản tiêu đề "TÀI KHOẢN ĐÃ ĐƯỢC CHIA SẼ"
+                                Text(
+                                    "THÔNG BÁO",
+                                    fontSize = 24.sp, // Font size linh hoạt
+                                    color = Color.White
+                                )
+                                Spacer(modifier = Modifier.heightIn(8.dp))
+                            }
+                        }
+                        // Box chứa góc lõm màu xám
+                        Box(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .wrapContentHeight() // Chiều cao linh hoạt theo LayoutConfig
+                        ) {
+                            // Box màu vàng nhỏ nằm trên góc phải
+                            Box(
+                                modifier = Modifier
+                                    .size(24.dp)
+                                    .align(Alignment.TopEnd)
+                                    .background(color = Color.Blue)
+                                    .zIndex(1f)  // Z-index thấp hơn
+                            )
+
+                            // Box màu xám bo tròn góc lõm trên cùng bên phải
+                            Box(
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .height(24.dp)
+                                    .background(
+                                        color = Color.LightGray,
+                                        shape = RoundedCornerShape(topEndPercent = 50)
+                                    )
+                                    .zIndex(2f), // Z-index cao hơn
+                                contentAlignment = Alignment.Center // Căn Row vào giữa Box
+                            ){}
+                        }
+                    }
+                }
+                NotificationScreen(notifications)
+            }
+        }
+    )
 }
 
