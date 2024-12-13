3f57fb6a889a6bb156f907d76dd072cab3a98e5f
phamanhtuan-coder <0306221391@caothang.edu.vn>
2024-12-14 01:12:26 +0700

    Temp Gradle for merge
    Signed-off-by: phamanhtuan-coder <0306221391@caothang.edu.vn>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,9 +41,6 @@ android {
 
 dependencies {
     implementation ("androidx.constraintlayout:constraintlayout-compose:1.0.1")
-    implementation ("androidx.compose.material:material-icons-core:1.4.0")
-    implementation ("androidx.compose.material:material-icons-extended:1.4.0")
-
     implementation("androidx.compose.foundation:foundation-layout:1.7.5")
     implementation ("androidx.compose.ui:ui:1.6.0")
     implementation ("androidx.compose.material:material:1.6.0")
