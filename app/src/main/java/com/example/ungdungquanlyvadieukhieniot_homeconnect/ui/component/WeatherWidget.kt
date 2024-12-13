b8db17258bbe440a6f789e776868efbe8fc77901
phamanhtuan-coder <0306221391@caothang.edu.vn>
2024-12-13 15:49:27 +0700

    Improve HomeScreen UI
    Signed-off-by: phamanhtuan-coder <0306221391@caothang.edu.vn>

--- app/src/main/java/com/example/ungdungquanlyvadieukhieniot_homeconnect/ui/component/WeatherWidget.kt
+++ app/src/main/java/com/example/ungdungquanlyvadieukhieniot_homeconnect/ui/component/WeatherWidget.kt
@@ -2,17 +2,18 @@ package com.example.ungdungquanlyvadieukhieniot_homeconnect.ui.component
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.lazy.grid.LazyHorizontalGrid
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
@@ -29,6 +30,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.platform.LocalConfiguration
@@ -36,25 +38,15 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import java.time.LocalTime.now
+import java.time.LocalTime
 
 /**
- * Giao diện thông tin thời tiết
+ * Weather Information UI
  * -----------------------------------------
- * Người viết:
- * - Phạm Anh Tuấn
- * Ngày viết:
- * - 11/12/2024
- * Lần cập nhật cuối:
- * - 11/12/2024
+ * Author: Phạm Anh Tuấn
+ * Created: 11/12/2024
+ * Last Updated: 13/12/2024
  * -----------------------------------------
- * Input:
- * - Không có
- *
- * Output:
- * - Card chứa thông tin thời tiết
- * ---------------------------------------
- *
  */
 
 @Preview(showBackground = true)
@@ -63,141 +55,124 @@ fun WeatherInfo() {
     val screenWidth = LocalConfiguration.current.screenWidthDp
     val isTablet = screenWidth > 600
 
-    // Temp
     val iconThoiGian =
-        if (isDayTime()) Icons.Filled.WbSunny else Icons.Filled.NightsStay // Ban ngày/ban đêm
-    val iconThoiTiet = Icons.Filled.WbSunny // Logic hình ảnh thời tiết
-    val thoiGianHienTai = "Sun, 12 Dec 2024 10:00 AM" // Thời gian hiện tại
-    val thoiTietHienTai = "Nắng" // Thời tiết hiện tại
-    val nhietDoHienTai = "25°C" // Nhiệt độ thực
-    val viTriHienTai = "Hanoi, Vietnam" //vị trí thực
-    val doAm = "80" // Độ ẩm, tầm nhìn, tốc độ gió
-    val tamNhin = "10" // Độ ẩm, tầm nhìn, tốc độ gió
-    val tocDoGio = "5" // Độ ẩm, tầm nhìn, tốc độ gió
+        if (isDayTime()) Icons.Filled.WbSunny else Icons.Filled.NightsStay
+    val iconThoiTiet = Icons.Filled.WbSunny
+    val thoiGianHienTai = "Sun, 12 Dec 2024 10:00 AM"
+    val thoiTietHienTai = "Sunny"
+    val nhietDoHienTai = "25°C"
+    val viTriHienTai = "Hanoi, Vietnam"
+    val doAm = "80"
+    val tamNhin = "10"
+    val tocDoGio = "5"
 
     Column(
         modifier = Modifier
             .fillMaxWidth()
             .background(
-                Color.Blue, shape = RoundedCornerShape(
-                    bottomStartPercent = 25,
-                    bottomEndPercent = 25
-                )
-            ),
-
-
-        ) {
+                brush = Brush.verticalGradient(
+                    colors = listOf(Color(0xFF6EC6FF), Color(0xFF2196F3))
+                ),
+                shape = RoundedCornerShape(bottomStartPercent = 25, bottomEndPercent = 25)
+            )
+            .padding(bottom = 16.dp)
+    ) {
         Column(
             modifier = Modifier
                 .padding(16.dp)
                 .background(
-                    Color.LightGray.copy(alpha = 3f), shape = RoundedCornerShape(
-                        25
-                    )
-                ),
-            verticalArrangement = Arrangement.SpaceEvenly,
+                    color = Color.White.copy(alpha = 0.7f),
+                    shape = RoundedCornerShape(25.dp)
+                )
+                .padding(16.dp),
+            verticalArrangement = Arrangement.SpaceBetween,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
             Row(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(vertical = 12.dp, horizontal = 16.dp),
+                modifier = Modifier.fillMaxWidth(),
                 horizontalArrangement = Arrangement.SpaceBetween,
                 verticalAlignment = Alignment.CenterVertically
             ) {
                 Icon(
                     imageVector = iconThoiTiet,
                     contentDescription = null,
-                    tint = Color.Gray,
-                    modifier = Modifier
-                        .size(if (isTablet) 80.dp else 30.dp)
-                        .align(Alignment.CenterVertically)
+                    tint = Color(0xFF424242),
+                    modifier = Modifier.size(if (isTablet) 80.dp else 40.dp)
                 )
                 Spacer(modifier = Modifier.width(8.dp))
-                Column {
+                Column(horizontalAlignment = Alignment.Start) {
                     Text(
                         text = thoiGianHienTai,
-                        color = Color.Black,
-                        fontSize = 16.sp
+                        color = Color(0xFF424242),
+                        fontSize = 14.sp
                     )
                     Text(
                         text = thoiTietHienTai,
-                        color = Color.Black,
+                        color = Color(0xFF424242),
                         fontSize = 20.sp,
                         fontWeight = FontWeight.Bold
                     )
                     Text(
                         text = viTriHienTai,
-                        color = Color.Black,
-                        fontSize = 16.sp
+                        color = Color(0xFF424242),
+                        fontSize = 14.sp
                     )
                 }
                 Spacer(modifier = Modifier.width(10.dp))
-                Column(
-                    horizontalAlignment = Alignment.CenterHorizontally
-                ) {
+                Column(horizontalAlignment = Alignment.CenterHorizontally) {
                     Text(
                         text = nhietDoHienTai,
-                        color = Color.Black,
+                        color = Color(0xFF424242),
                         fontSize = if (isTablet) 40.sp else 30.sp,
                         fontWeight = FontWeight.Bold
                     )
                     Icon(
                         imageVector = iconThoiGian,
                         contentDescription = null,
-                        tint = Color.Gray,
-                        modifier = if (isTablet) Modifier.size(32.dp) else Modifier.size(28.dp)
+                        tint = Color(0xFF424242),
+                        modifier = Modifier.size(if (isTablet) 32.dp else 28.dp)
                     )
                 }
             }
 
-
             HorizontalDivider(
-                modifier = Modifier.padding(horizontal = 8.dp),
-                thickness = 2.dp,
-                color = Color.Black
+                modifier = Modifier.padding(vertical = 16.dp),
+                thickness = 1.dp,
+                color = Color(0xFFBDBDBD)
             )
-
-
             LazyVerticalGrid(
                 modifier = Modifier
-                    .height(if (isTablet) 100.dp else 90.dp)
-                    .padding(horizontal = 5.dp)
-                    .fillMaxWidth(),
-                horizontalArrangement = Arrangement.SpaceEvenly,
-                verticalArrangement = Arrangement.Center,
-                columns = GridCells.Fixed(3)
+                    .fillMaxWidth()
+                    .heightIn(max = 100.dp),
+                columns = GridCells.Fixed(3),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalArrangement = Arrangement.Center
             ) {
                 item {
-
-
                     WeatherWidget(
                         icon = Icons.Filled.WaterDrop,
-                        value = doAm + "%",
+                        value = "$doAm%",
                         label = "Độ ẩm",
                         isTablet = isTablet
                     )
-
                 }
                 item {
                     WeatherWidget(
                         icon = Icons.Filled.Visibility,
-                        value = tamNhin + " km",
+                        value = "$tamNhin km",
                         label = "Tầm nhìn",
                         isTablet = isTablet
                     )
                 }
                 item {
-
                     WeatherWidget(
                         icon = Icons.Filled.Air,
-                        value = tocDoGio + " km/h",
-                        label = "Tốc độ gió",
+                        value = "$tocDoGio km/h",
+                        label = "Phong tốc",
                         isTablet = isTablet
                     )
                 }
             }
-            Spacer(modifier = Modifier.height(8.dp))
 
         }
     }
@@ -207,88 +182,67 @@ fun WeatherInfo() {
 fun WeatherWidget(icon: ImageVector, value: String, label: String, isTablet: Boolean) {
     Card(
         modifier = Modifier
-            .width(if (isTablet) 240.dp else 100.dp)
-            .height(if (isTablet) 100.dp else 90.dp)
-            .padding(8.dp)
-            .background(
-                Color.Transparent,
-                shape = RoundedCornerShape(25.dp),
-            ),
-
-        shape = RoundedCornerShape(25.dp),
-        elevation = CardDefaults.cardElevation(0.dp),
+            .padding(if (isTablet) 12.dp else 5.dp)
+            .width(if (isTablet) 150.dp else 100.dp)
+            .height(if (isTablet) 80.dp else 45.dp),
         colors = CardDefaults.cardColors(
-            containerColor = Color.White.copy(alpha = 0.2f) // Glass effect
-        )
+            containerColor = Color.White.copy(alpha = 0.8f)
+        ),
+        elevation = CardDefaults.cardElevation(4.dp),
+        shape = RoundedCornerShape(15.dp)
     ) {
-        if (isTablet) {
-            Row(
+        Row(
+            modifier = Modifier
+                .background(
+                    color = Color.White.copy(alpha = 0.8f),
+                    shape = RoundedCornerShape(15.dp)
+                )
+                .fillMaxSize()
+                .padding(horizontal = if (isTablet) 16.dp else 4.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Start
+        ) {
+            // Icon Box
+            Box(
                 modifier = Modifier
-                    .padding(vertical = 8.dp, horizontal = 16.dp)
-                    .fillMaxSize(),
-                horizontalArrangement = Arrangement.Center,
-                verticalAlignment = Alignment.CenterVertically
+                    .size(if (isTablet) 45.dp else 22.dp),
+                contentAlignment = Alignment.Center
             ) {
                 Icon(
                     imageVector = icon,
                     contentDescription = null,
-                    tint = Color.Gray,
-                    modifier = Modifier
-                        .size(50.dp)
+                    tint = Color(0xFF424242),
+                    modifier = Modifier.fillMaxSize()
                 )
-                Spacer(modifier = Modifier.width(16.dp))
-                Column(
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                    verticalArrangement = Arrangement.Center
-                ) {
-                    Text(
-                        text = label,
-                        color = Color.White,
-                        fontSize = 16.sp,
-                        fontWeight = FontWeight.Bold
-                    )
-                    Text(
-                        text = value,
-                        color = Color.White,
-                        fontSize = 20.sp
-                    )
-                }
-
             }
-        } else {
+
+            Spacer(modifier = Modifier.width(8.dp))
+
+            // Constrain the Column Layout
             Column(
                 modifier = Modifier
-                    .padding(0.dp)
-                    .fillMaxSize(),
-                horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.Center
+                    .fillMaxWidth(),// Prevents unnecessary stretching
+                verticalArrangement = Arrangement.Center, // Keep the items close together
+                horizontalAlignment = Alignment.Start
             ) {
-                Icon(
-                    imageVector = icon,
-                    contentDescription = null,
-                    tint = Color.Gray,
-                    modifier = Modifier
-                        .size(24.dp)
-                )
                 Text(
                     text = label,
-                    color = Color.White,
-                    fontSize = 14.sp,
+                    color = Color(0xFF424242),
+                    fontSize = if (isTablet) 24.sp else 13.sp,
                     fontWeight = FontWeight.Bold
                 )
                 Text(
                     text = value,
-                    color = Color.White,
-                    fontSize = 16.sp
+                    color = Color(0xFF424242),
+                    fontSize = if (isTablet) 20.sp else 12.sp,
                 )
             }
-
         }
-
     }
 }
 
+
 fun isDayTime(): Boolean {
-    val hour = now().hour
+    val hour = LocalTime.now().hour
     return hour in 6..18
 }
