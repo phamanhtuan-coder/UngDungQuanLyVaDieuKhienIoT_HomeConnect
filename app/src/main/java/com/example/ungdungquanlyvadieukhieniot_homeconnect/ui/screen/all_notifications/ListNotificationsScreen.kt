a2243f21ca39c47c5847bf9b380909d4bd034b63
phamanhtuan-coder <0306221391@caothang.edu.vn>
2024-12-31 01:21:44 +0700

    Update giao diện all notification
    Signed-off-by: phamanhtuan-coder <0306221391@caothang.edu.vn>

--- app/src/main/java/com/example/ungdungquanlyvadieukhieniot_homeconnect/ui/screen/all_notifications/ListNotificationsScreen.kt
+++ app/src/main/java/com/example/ungdungquanlyvadieukhieniot_homeconnect/ui/screen/all_notifications/ListNotificationsScreen.kt
@@ -10,12 +10,11 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
@@ -25,34 +24,18 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.foundation.lazy.items
-import androidx.compose.material.icons.filled.Search
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.FabPosition
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.TextField
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.compose.ui.zIndex
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
-import com.example.ungdungquanlyvadieukhieniot_homeconnect.ui.component.Header
-import com.example.ungdungquanlyvadieukhieniot_homeconnect.ui.component.MenuBottom
-import com.example.ungdungquanlyvadieukhieniot_homeconnect.ui.component.NutHome
-
 
 
 private object NotificationStyle {
@@ -101,13 +84,15 @@ fun NotificationScreen(
         containerColor = Color.LightGray,
         topBar = {
 
-                 },
+        },
         bottomBar = {
 
-                    },
+        },
         content = { innerPadding ->
             Box(
-                modifier = Modifier.fillMaxSize().padding(innerPadding)
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(innerPadding)
             ) {
                 if (notifications.isEmpty()) {
                     EmptyNotificationScreen()
@@ -366,12 +351,11 @@ data class Notification(
 //}
 
 
-@Preview (showBackground = true, showSystemUi = true)
+@Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun PreviewNotification() {
     NotificationScreen(
-        rememberNavController()
-        ,listOf(
+        rememberNavController(), listOf(
             Notification("New Feature Alert!", "We've introduced new features.", false),
             Notification("System Update", "Your app has been updated.", true),
             Notification("Reminder", "Your meeting is scheduled at 3 PM.", false)
@@ -379,11 +363,10 @@ fun PreviewNotification() {
     )
 }
 
-@Preview (showBackground = true, showSystemUi = true)
+@Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun PreviewEmptyNotification() {
     NotificationScreen(
-        rememberNavController()
-        ,listOf()
+        rememberNavController(), listOf()
     )
 }
