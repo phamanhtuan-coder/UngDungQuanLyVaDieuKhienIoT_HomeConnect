38cfd253d0c6f446379e58b739a94e691aac8c2c
Nguyễn Thanh Sang <nguyenthanhsang92643@gmail.com>
2025-01-12 18:26:32 +0700

    Thêm phần gửi register

--- app/src/main/java/com/example/ungdungquanlyvadieukhieniot_homeconnect/data/repository/AuthRepository.kt
+++ app/src/main/java/com/example/ungdungquanlyvadieukhieniot_homeconnect/data/repository/AuthRepository.kt
@@ -3,6 +3,9 @@ package com.example.ungdungquanlyvadieukhieniot_homeconnect.data.repository
 import com.example.ungdungquanlyvadieukhieniot_homeconnect.data.remote.api.RetrofitClient
 import com.example.ungdungquanlyvadieukhieniot_homeconnect.data.remote.dto.LoginRequest
 import com.example.ungdungquanlyvadieukhieniot_homeconnect.data.remote.dto.LoginResponse
+import com.example.ungdungquanlyvadieukhieniot_homeconnect.data.remote.dto.RegisterRequest
+import com.example.ungdungquanlyvadieukhieniot_homeconnect.data.remote.dto.RegisterResponse
+import com.example.ungdungquanlyvadieukhieniot_homeconnect.data.remote.dto.User
 
 class AuthRepository {
     private val apiService = RetrofitClient.apiService
@@ -15,5 +18,20 @@ class AuthRepository {
         )
         // Gọi API
         return apiService.login(request)
+    }
+
+    suspend fun register(user: RegisterRequest): RegisterResponse {
+        // Tạo request
+        val request = RegisterRequest(
+            Name = user.Name,
+            Email = user.Email,
+            PasswordHash = user.PasswordHash,
+            Phone = user.Phone,
+            Address = user.Address,
+            DateOfBirth = user.DateOfBirth,
+            ProfileImage = user.ProfileImage,
+        )
+        // Gọi API
+        return apiService.register(request)
     }
 }
