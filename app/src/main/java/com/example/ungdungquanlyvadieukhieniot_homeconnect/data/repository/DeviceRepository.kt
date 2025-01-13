87768214bdbcaccc8853e8f6720aae5c0f01acb4
Nguyễn Thanh Sang <nguyenthanhsang92643@gmail.com>
2025-01-13 11:41:19 +0700

    Thêm xử lý trong DeviceRepository.kt

--- app/src/main/java/com/example/ungdungquanlyvadieukhieniot_homeconnect/data/repository/DeviceRepository.kt
+++ app/src/main/java/com/example/ungdungquanlyvadieukhieniot_homeconnect/data/repository/DeviceRepository.kt
@@ -3,6 +3,8 @@ package com.example.ungdungquanlyvadieukhieniot_homeconnect.data.repository
 import android.content.Context
 import com.example.ungdungquanlyvadieukhieniot_homeconnect.data.remote.api.RetrofitClient
 import com.example.ungdungquanlyvadieukhieniot_homeconnect.data.remote.dto.DeviceResponse
+import com.example.ungdungquanlyvadieukhieniot_homeconnect.data.remote.dto.ToggleRequest
+import com.example.ungdungquanlyvadieukhieniot_homeconnect.data.remote.dto.ToggleResponse
 
 class DeviceRepository (private val context: Context) {
     private val apiService = RetrofitClient.apiService
@@ -11,5 +13,11 @@ class DeviceRepository (private val context: Context) {
         val sharedPrefs =context.getSharedPreferences("MY_APP_PREFS", Context.MODE_PRIVATE)
         val token=  sharedPrefs.getString("JWT_TOKEN", "") ?: ""
         return apiService.getInfoDevice(deviceId, token = "Bearer $token")
+    }
+
+    suspend fun postToggleDevice(deviceId: Int, toggleRequest: ToggleRequest): ToggleResponse {
+        val sharedPrefs =context.getSharedPreferences("MY_APP_PREFS", Context.MODE_PRIVATE)
+        val token=  sharedPrefs.getString("JWT_TOKEN", "") ?: ""
+        return apiService.toggleDevice(deviceId, toggleRequest, token = "Bearer $token")
     }
 }
