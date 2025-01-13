53f69c245fe020de9a4a9ce691148b48bc41cdde
Nguyễn Thanh Sang <nguyenthanhsang92643@gmail.com>
2025-01-13 11:41:33 +0700

    Thêm xử lý trong FireAlarmDetailViewModel.kt

--- app/src/main/java/com/example/ungdungquanlyvadieukhieniot_homeconnect/ui/screen/device_detail_for_fire_alarm/FireAlarmDetailViewModel.kt
+++ app/src/main/java/com/example/ungdungquanlyvadieukhieniot_homeconnect/ui/screen/device_detail_for_fire_alarm/FireAlarmDetailViewModel.kt
@@ -6,6 +6,8 @@ import android.util.Log
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.ungdungquanlyvadieukhieniot_homeconnect.data.remote.dto.DeviceResponse
+import com.example.ungdungquanlyvadieukhieniot_homeconnect.data.remote.dto.ToggleRequest
+import com.example.ungdungquanlyvadieukhieniot_homeconnect.data.remote.dto.ToggleResponse
 import com.example.ungdungquanlyvadieukhieniot_homeconnect.data.repository.DeviceRepository
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
@@ -16,6 +18,13 @@ sealed class getInfoDeviceState {
     object Loading : getInfoDeviceState()            // Đang loading
     data class Success(val device: DeviceResponse) : getInfoDeviceState()
     data class Error(val error: String) : getInfoDeviceState()
+}
+
+sealed class toggletate {
+    object Idle : toggletate()               // Chưa làm gì
+    object Loading : toggletate()            // Đang loading
+    data class Success(val toggle: ToggleResponse) : toggletate()
+    data class Error(val error: String) : toggletate()
 }
 
 class FireAlarmDetailViewModel(application: Application, context: Context) : AndroidViewModel(application) {
@@ -36,6 +45,23 @@ class FireAlarmDetailViewModel(application: Application, context: Context) : And
             } catch (e: Exception) {
                 Log.e("DeviceViewModel", "Error fetching spaces: ${e.message}")
                 _infoDeviceState.value = com.example.ungdungquanlyvadieukhieniot_homeconnect.ui.screen.device_detail.getInfoDeviceState.Error(e.message ?: "Danh sach load thất bại!")
+            }
+        }
+    }
+
+    private val _toggleState = MutableStateFlow<toggletate>(toggletate.Idle)
+    val toggleState = _toggleState.asStateFlow()
+
+    fun toggleDevice(deviceId: Int, toggle: ToggleRequest) {
+        viewModelScope.launch {
+            try {
+                _toggleState.value = toggletate.Loading
+                val response = repository.postToggleDevice(deviceId,toggle)
+                _toggleState.value = toggletate.Success(response)
+                Log.d("API Response", toggle.toString())
+            } catch (e: Exception) {
+                Log.e("DeviceDetailViewModel", "Error fetching toggle: ${e.message}")
+                _toggleState.value = toggletate.Error(e.message ?: "Load thất bại!")
             }
         }
     }
