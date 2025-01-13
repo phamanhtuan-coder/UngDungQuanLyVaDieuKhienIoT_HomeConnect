9beb319bea821a5c02d32d955579b593f39a3414
Nguyễn Thanh Sang <nguyenthanhsang92643@gmail.com>
2025-01-13 09:28:33 +0700

    Thêm file class và xữ lý DeviceDetailViewModel.kt

---
+++ app/src/main/java/com/example/ungdungquanlyvadieukhieniot_homeconnect/ui/screen/device_detail/DeviceDetailViewModel.kt
@@ -0,0 +1,43 @@
+package com.example.ungdungquanlyvadieukhieniot_homeconnect.ui.screen.device_detail
+
+import android.app.Application
+import android.content.Context
+import android.util.Log
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.ungdungquanlyvadieukhieniot_homeconnect.data.remote.dto.DeviceResponse
+import com.example.ungdungquanlyvadieukhieniot_homeconnect.data.remote.dto.RegisterRequest
+import com.example.ungdungquanlyvadieukhieniot_homeconnect.data.repository.DeviceRepository
+import com.example.ungdungquanlyvadieukhieniot_homeconnect.data.repository.SpaceRepository
+import com.example.ungdungquanlyvadieukhieniot_homeconnect.ui.screen.device.SpaceState
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+
+sealed class getInfoDeviceState {
+    object Idle : getInfoDeviceState()               // Chưa làm gì
+    object Loading : getInfoDeviceState()            // Đang loading
+    data class Success(val device: DeviceResponse) : getInfoDeviceState()
+    data class Error(val error: String) : getInfoDeviceState()
+}
+class DeviceDetailViewModel(application: Application, context: Context) : AndroidViewModel(application) {
+    private val repository = DeviceRepository(context) // Repository để quản lý cả Spaces và Devices
+
+    private val _infoDeviceState = MutableStateFlow<getInfoDeviceState>(getInfoDeviceState.Idle)
+    val infoDeviceState = _infoDeviceState.asStateFlow()
+    /**
+     * Lấy danh sách Spaces theo Home ID
+     */
+    fun getInfoDevice(deviceId: Int) {
+        viewModelScope.launch {
+            try {
+                _infoDeviceState.value = getInfoDeviceState.Loading
+                val response = repository.getInfoDevice(deviceId)
+                _infoDeviceState.value = getInfoDeviceState.Success(response)
+            } catch (e: Exception) {
+                Log.e("DeviceViewModel", "Error fetching spaces: ${e.message}")
+                _infoDeviceState.value = getInfoDeviceState.Error(e.message ?: "Danh sach load thất bại!")
+            }
+        }
+    }
+}
