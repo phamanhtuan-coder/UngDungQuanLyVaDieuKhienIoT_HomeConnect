7fd547884ad5f9b09f5ef194add712414e07531d
Nguyễn Thanh Sang <nguyenthanhsang92643@gmail.com>
2025-01-13 09:27:43 +0700

    Chỉnh sửa DeviceResponse.kt

--- app/src/main/java/com/example/ungdungquanlyvadieukhieniot_homeconnect/data/remote/dto/DeviceResponse.kt
+++ app/src/main/java/com/example/ungdungquanlyvadieukhieniot_homeconnect/data/remote/dto/DeviceResponse.kt
@@ -3,5 +3,6 @@ package com.example.ungdungquanlyvadieukhieniot_homeconnect.data.remote.dto
 data class DeviceResponse(
     val DeviceID: Int,
     val TypeID: Int,
-    val Name: String
+    val Name: String,
+    val PowerStatus: Boolean
 )
