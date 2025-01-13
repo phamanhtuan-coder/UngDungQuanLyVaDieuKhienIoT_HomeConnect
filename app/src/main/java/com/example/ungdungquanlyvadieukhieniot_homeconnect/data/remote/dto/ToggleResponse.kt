d6dd97daf006ddd19ffeb418d4cf93f19e3cf956
Nguyễn Thanh Sang <nguyenthanhsang92643@gmail.com>
2025-01-13 19:29:03 +0700

    Cập nhật device trong ToggleResponse.kt

--- app/src/main/java/com/example/ungdungquanlyvadieukhieniot_homeconnect/data/remote/dto/ToggleResponse.kt
+++ app/src/main/java/com/example/ungdungquanlyvadieukhieniot_homeconnect/data/remote/dto/ToggleResponse.kt
@@ -2,6 +2,6 @@ package com.example.ungdungquanlyvadieukhieniot_homeconnect.data.remote.dto
 
 data class ToggleResponse(
     val message: String,
-    val device: DeviceResponse,
+    var device: DeviceResponse,
     val error: String
 )
