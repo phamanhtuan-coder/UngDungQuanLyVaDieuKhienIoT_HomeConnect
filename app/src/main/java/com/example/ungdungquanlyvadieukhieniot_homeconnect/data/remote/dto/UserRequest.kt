42bc501081fe9e97bff6c5913a8d3110dc8acb4f
Nguyễn Thanh Sang <nguyenthanhsang92643@gmail.com>
2025-01-15 14:22:11 +0700

    Thêm data class trong UserRequest.kt

---
+++ app/src/main/java/com/example/ungdungquanlyvadieukhieniot_homeconnect/data/remote/dto/UserRequest.kt
@@ -0,0 +1,9 @@
+package com.example.ungdungquanlyvadieukhieniot_homeconnect.data.remote.dto
+
+data class UserRequest(
+    val Name: String,
+    val Email: String,
+    val Phone: String,
+    val Address: String,
+    val DateOfBirth: String,
+)
