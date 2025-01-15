354b61ab53c464c6e0ce0a7e318adcf1fddd0690
Nguyễn Thanh Sang <nguyenthanhsang92643@gmail.com>
2025-01-15 14:21:01 +0700

    Bổ sung User.kt

--- app/src/main/java/com/example/ungdungquanlyvadieukhieniot_homeconnect/data/remote/dto/User.kt
+++ app/src/main/java/com/example/ungdungquanlyvadieukhieniot_homeconnect/data/remote/dto/User.kt
@@ -4,5 +4,10 @@ data class User(
     val UserID: Int,
     val Name: String,
     val Email: String,
+    val Phone: String,
+    val Address: String,
+    val DateOfBirth: String,
+    val EmailVerified: Boolean,
+    val ProfileImage: String
     // Todo: ... các trường khác nếu cần
 )
