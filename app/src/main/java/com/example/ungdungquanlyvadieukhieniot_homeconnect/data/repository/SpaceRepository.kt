18669f1e12a7e68a37e51f354c8e44f4a3ca5cd3
Nguyễn Thanh Sang <nguyenthanhsang92643@gmail.com>
2025-01-17 03:49:16 +0700

    Bổ sung code SpaceRepository.kt

--- app/src/main/java/com/example/ungdungquanlyvadieukhieniot_homeconnect/data/repository/SpaceRepository.kt
+++ app/src/main/java/com/example/ungdungquanlyvadieukhieniot_homeconnect/data/repository/SpaceRepository.kt
@@ -2,9 +2,14 @@ package com.example.ungdungquanlyvadieukhieniot_homeconnect.data.repository
 
 import android.app.Application
 import android.content.Context
+import android.util.Log
+import com.example.ungdungquanlyvadieukhieniot_homeconnect.data.remote.api.ApiService.UpdateSpaceRequest
 import com.example.ungdungquanlyvadieukhieniot_homeconnect.data.remote.api.RetrofitClient
+import com.example.ungdungquanlyvadieukhieniot_homeconnect.data.remote.dto.CreateSpaceRequest
+import com.example.ungdungquanlyvadieukhieniot_homeconnect.data.remote.dto.CreateSpaceResponse
 import com.example.ungdungquanlyvadieukhieniot_homeconnect.data.remote.dto.DeviceResponse
 import com.example.ungdungquanlyvadieukhieniot_homeconnect.data.remote.dto.SpaceResponse
+import com.example.ungdungquanlyvadieukhieniot_homeconnect.data.remote.dto.SpaceResponse2
 
 class SpaceRepository(private val context: Context) {
     private val apiService = RetrofitClient.apiService
@@ -19,5 +24,41 @@ class SpaceRepository(private val context: Context) {
         val sharedPrefs =context.getSharedPreferences("MY_APP_PREFS", Context.MODE_PRIVATE)
         val token=  sharedPrefs.getString("JWT_TOKEN", "") ?: ""
         return apiService.getDevices(spaceId, token = "Bearer $token")
+    }
+
+    suspend fun getSpaces(houseId: Int): List<SpaceResponse2> {
+        val sharedPrefs = context.getSharedPreferences("MY_APP_PREFS", Context.MODE_PRIVATE)
+        val token = sharedPrefs.getString("JWT_TOKEN", "") ?: ""
+        return apiService.getSpaces(houseId, "Bearer $token")
+    }
+
+    suspend fun updateSpace(spaceId: Int, name: String): SpaceResponse3 {
+        val sharedPrefs = context.getSharedPreferences("MY_APP_PREFS", Context.MODE_PRIVATE)
+        val token = sharedPrefs.getString("JWT_TOKEN", "") ?: ""
+
+        // Log để kiểm tra dữ liệu trước khi gọi API
+        Log.d("UpdateSpaceRepository", "Token: Bearer $token")
+        Log.d("UpdateSpaceRepository", "Payload: SpaceID = $spaceId, Name = $name")
+
+        return apiService.updateSpace(
+            spaceId = spaceId,
+            body = UpdateSpaceRequest(Name = name),
+            token = "Bearer $token"
+        )
+    }
+
+    suspend fun createSpace(houseId: Int, name: String): CreateSpaceResponse {
+        val sharedPrefs = context.getSharedPreferences("MY_APP_PREFS", Context.MODE_PRIVATE)
+        val token = sharedPrefs.getString("JWT_TOKEN", "") ?: ""
+
+        Log.d("CreateSpaceRepository", "Token: Bearer $token")
+        Log.d("CreateSpaceRepository", "Payload: HouseID = $houseId, Name = $name")
+
+        return apiService.createSpace(
+            body = CreateSpaceRequest(HouseID = houseId, Name = name),
+            token = "Bearer $token"
+        ).also {
+            Log.d("CreateSpaceRepository", "API Response: ${it.message}, Space: ${it.space}")
+        }
     }
 }
