5d18337fefca13d75aa96ae5fba6bb9fb37d7d39
Nguyễn Thanh Sang <nguyenthanhsang92643@gmail.com>
2025-01-17 00:12:25 +0700

    bổ sung getHouses

--- app/src/main/java/com/example/ungdungquanlyvadieukhieniot_homeconnect/data/repository/HouseRepository.kt
+++ app/src/main/java/com/example/ungdungquanlyvadieukhieniot_homeconnect/data/repository/HouseRepository.kt
@@ -3,6 +3,7 @@ package com.example.ungdungquanlyvadieukhieniot_homeconnect.data.repository
 import android.content.Context
 import com.example.ungdungquanlyvadieukhieniot_homeconnect.data.remote.api.RetrofitClient
 import com.example.ungdungquanlyvadieukhieniot_homeconnect.data.remote.dto.HouseResponse
+import com.example.ungdungquanlyvadieukhieniot_homeconnect.data.remote.dto.HousesListPesponse
 import com.example.ungdungquanlyvadieukhieniot_homeconnect.data.remote.dto.SpaceResponse
 
 class HouseRepository (private val context: Context){
@@ -12,5 +13,14 @@ class HouseRepository (private val context: Context){
         val sharedPrefs =context.getSharedPreferences("MY_APP_PREFS", Context.MODE_PRIVATE)
         val token=  sharedPrefs.getString("JWT_TOKEN", "") ?: ""
         return apiService.getListHome(token = "Bearer $token")
+    }
+
+    suspend fun getHouses(): List<HousesListPesponse> {
+        // Lấy token từ SharedPreferences
+        val sharedPrefs = context.getSharedPreferences("MY_APP_PREFS", Context.MODE_PRIVATE)
+        val token = sharedPrefs.getString("JWT_TOKEN", "") ?: ""
+
+        // Gọi API với token
+        return apiService.getHouses(token = "Bearer $token")
     }
 }
