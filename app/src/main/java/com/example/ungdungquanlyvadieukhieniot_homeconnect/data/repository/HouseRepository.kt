3ea0db55e5db484881a864080f0eaf766c4e08cf
Nguyễn Thanh Sang <nguyenthanhsang92643@gmail.com>
2025-01-17 00:37:09 +0700

    bổ sung hàm updateHouse trong HouseRepository.kt

--- app/src/main/java/com/example/ungdungquanlyvadieukhieniot_homeconnect/data/repository/HouseRepository.kt
+++ app/src/main/java/com/example/ungdungquanlyvadieukhieniot_homeconnect/data/repository/HouseRepository.kt
@@ -5,6 +5,8 @@ import com.example.ungdungquanlyvadieukhieniot_homeconnect.data.remote.api.Retro
 import com.example.ungdungquanlyvadieukhieniot_homeconnect.data.remote.dto.HouseResponse
 import com.example.ungdungquanlyvadieukhieniot_homeconnect.data.remote.dto.HousesListPesponse
 import com.example.ungdungquanlyvadieukhieniot_homeconnect.data.remote.dto.SpaceResponse
+import com.example.ungdungquanlyvadieukhieniot_homeconnect.data.remote.dto.UpdateHouseRequest
+import com.example.ungdungquanlyvadieukhieniot_homeconnect.data.remote.dto.UpdateHouseResponse
 
 class HouseRepository (private val context: Context){
     private val apiService = RetrofitClient.apiService
@@ -22,5 +24,14 @@ class HouseRepository (private val context: Context){
 
         // Gọi API với token
         return apiService.getHouses(token = "Bearer $token")
+    }
+
+    suspend fun updateHouse(houseID: Int, request: UpdateHouseRequest): UpdateHouseResponse {
+        // Lấy token từ SharedPreferences
+        val sharedPrefs = context.getSharedPreferences("MY_APP_PREFS", Context.MODE_PRIVATE)
+        val token = sharedPrefs.getString("JWT_TOKEN", "") ?: ""
+
+        // Gọi API với token
+        return apiService.updateHouse(houseID, request, token = "Bearer $token")
     }
 }
