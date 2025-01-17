befe26ad18ab602fbddcaeec40749d8172a4adcd
Nguyễn Thanh Sang <nguyenthanhsang92643@gmail.com>
2025-01-17 14:05:54 +0700

    Bổ sung DashboardViewModel.kt

--- app/src/main/java/com/example/ungdungquanlyvadieukhieniot_homeconnect/ui/screen/dashboard/DashboardViewModel.kt
+++ app/src/main/java/com/example/ungdungquanlyvadieukhieniot_homeconnect/ui/screen/dashboard/DashboardViewModel.kt
@@ -1,102 +1,47 @@
 package com.example.ungdungquanlyvadieukhieniot_homeconnect.ui.screen.dashboard
 
-import android.app.Application
-import android.content.Context
-import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.example.ungdungquanlyvadieukhieniot_homeconnect.data.remote.dto.DailySensorRequest
-import com.example.ungdungquanlyvadieukhieniot_homeconnect.data.remote.dto.DeviceResponse
-import com.example.ungdungquanlyvadieukhieniot_homeconnect.data.remote.dto.RangeSensorRequest
-import com.example.ungdungquanlyvadieukhieniot_homeconnect.data.remote.dto.WeeklySensorRequest
+import com.example.ungdungquanlyvadieukhieniot_homeconnect.data.remote.dto.PowerUsageData
+import com.example.ungdungquanlyvadieukhieniot_homeconnect.data.remote.dto.SensorAverageData
 import com.example.ungdungquanlyvadieukhieniot_homeconnect.data.repository.StatisticsRepository
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 
-sealed class DashboardState {
-    object Idle : DashboardState()               // Chưa làm gì
-    object Loading : DashboardState()            // Đang loading
-    data class Success(val deviceList: List<DeviceResponse>) :DashboardState()
-    data class Error(val error: String) : DashboardState()
+sealed class StatisticsState {
+    object Idle : StatisticsState()
+    object Loading : StatisticsState()
+    data class PowerUsageSuccess(val message: String, val data: List<PowerUsageData>) : StatisticsState()
+    data class SensorAveragesSuccess(val message: String, val data: List<SensorAverageData>) : StatisticsState()
+    data class Error(val error: String) : StatisticsState()
 }
 
-sealed class DailyStatisticsState {
-    object Idle : DailyStatisticsState()
-    object Loading : DailyStatisticsState()
-    data class Success<T>(val data: T) : DailyStatisticsState()
-    data class Error(val error: String) : DailyStatisticsState()
-}
+class StatisticsViewModel(val repository: StatisticsRepository) : ViewModel() {
 
-sealed class WeeklyStatisticsState {
-    object Idle : WeeklyStatisticsState()
-    object Loading : WeeklyStatisticsState()
-    data class Success<T>(val data: T) : WeeklyStatisticsState()
-    data class Error(val error: String) : WeeklyStatisticsState()
-}
+    private val _statisticsState = MutableStateFlow<StatisticsState>(StatisticsState.Idle)
+    val statisticsState = _statisticsState.asStateFlow()
 
-sealed class RangeStatisticsState {
-    object Idle : RangeStatisticsState()
-    object Loading : RangeStatisticsState()
-    data class Success<T>(val data: T) : RangeStatisticsState()
-    data class Error(val error: String) : RangeStatisticsState()
-}
-
-class DashboardViewModel(application: Application, context: Context) : AndroidViewModel(application) {
-    private val repository = StatisticsRepository(context)
-
-    private val _dailyStatisticsState = MutableStateFlow<DailyStatisticsState>(DailyStatisticsState.Idle)
-    val dailyStatisticsState = _dailyStatisticsState.asStateFlow()
-
-    private val _weeklyStatisticsState = MutableStateFlow<WeeklyStatisticsState>(WeeklyStatisticsState.Idle)
-    val weeklyStatisticsState = _weeklyStatisticsState.asStateFlow()
-
-    private val _rangeStatisticsState = MutableStateFlow<RangeStatisticsState>(RangeStatisticsState.Idle)
-    val rangeStatisticsState = _rangeStatisticsState.asStateFlow()
-
-    fun calculateDailyAverageSensor(request: DailySensorRequest) {
-        _dailyStatisticsState.value = DailyStatisticsState.Loading
+    fun fetchDailyRoomPowerUsage(spaceId: Int, startDate: String, endDate: String) {
+        _statisticsState.value = StatisticsState.Loading
         viewModelScope.launch {
             try {
-                val response = repository.calculateDailyAverageSensor(request)
-                _dailyStatisticsState.value = DailyStatisticsState.Success(response)
+                val response = repository.getDailyRoomPowerUsage(spaceId, startDate, endDate)
+                _statisticsState.value = StatisticsState.PowerUsageSuccess(message = response.message, response.data)
             } catch (e: Exception) {
-                _dailyStatisticsState.value = DailyStatisticsState.Error(e.message ?: "Error calculating daily average sensor")
+                _statisticsState.value = StatisticsState.Error(e.message ?: "Đã xảy ra lỗi không xác định")
             }
         }
     }
 
-    fun calculateWeeklyAverageSensor(request: WeeklySensorRequest) {
-        _weeklyStatisticsState.value = WeeklyStatisticsState.Loading
+    fun fetchDailyRoomAveragesSensor(spaceId: Int, startDate: String, endDate: String) {
+        _statisticsState.value = StatisticsState.Loading
         viewModelScope.launch {
             try {
-                val response = repository.calculateWeeklyAverageSensor(request)
-                _weeklyStatisticsState.value = WeeklyStatisticsState.Success(response)
+                val response = repository.getDailyRoomAveragesSensor(spaceId, startDate, endDate)
+                _statisticsState.value = StatisticsState.SensorAveragesSuccess(response.message, response.data)
             } catch (e: Exception) {
-                _weeklyStatisticsState.value = WeeklyStatisticsState.Error(e.message ?: "Error calculating weekly average sensor")
-            }
-        }
-    }
-
-    fun calculateAverageSensorForRange(request: RangeSensorRequest) {
-        _rangeStatisticsState.value = RangeStatisticsState.Loading
-        viewModelScope.launch {
-            try {
-                val response = repository.calculateAverageSensorForRange(request)
-                _rangeStatisticsState.value = RangeStatisticsState.Success(response)
-            } catch (e: Exception) {
-                _rangeStatisticsState.value = RangeStatisticsState.Error(e.message ?: "Error calculating average sensor for range")
-            }
-        }
-    }
-
-    fun getWeeklyAverageSensor(deviceId: Int) {
-        _weeklyStatisticsState.value = WeeklyStatisticsState.Loading
-        viewModelScope.launch {
-            try {
-                val response = repository.getWeeklyAverageSensor(deviceId)
-                _weeklyStatisticsState.value = WeeklyStatisticsState.Success(response)
-            } catch (e: Exception) {
-                _weeklyStatisticsState.value = WeeklyStatisticsState.Error(e.message ?: "Error fetching weekly average sensor")
+                _statisticsState.value = StatisticsState.Error(e.message ?: "Đã xảy ra lỗi không xác định")
             }
         }
     }
