6640f58c2de7c0e5a8b020fedd5ffa456f9ca334
Nguyễn Thanh Sang <nguyenthanhsang92643@gmail.com>
2025-01-18 00:25:55 +0700

    Thêm HomeScreen.kt

--- app/src/main/java/com/example/ungdungquanlyvadieukhieniot_homeconnect/ui/screen/home/HomeScreen.kt
+++ app/src/main/java/com/example/ungdungquanlyvadieukhieniot_homeconnect/ui/screen/home/HomeScreen.kt
@@ -1,6 +1,9 @@
 package com.example.ungdungquanlyvadieukhieniot_homeconnect.ui.screen.home
 
+import android.app.Application
+import android.util.Log
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -8,24 +11,35 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
-import androidx.navigation.compose.rememberNavController
+import com.example.ungdungquanlyvadieukhieniot_homeconnect.data.remote.dto.SharedWithResponse
 import com.example.ungdungquanlyvadieukhieniot_homeconnect.ui.component.DeviceCard
 import com.example.ungdungquanlyvadieukhieniot_homeconnect.ui.component.Header
 import com.example.ungdungquanlyvadieukhieniot_homeconnect.ui.component.HouseSelection
@@ -55,6 +69,38 @@ fun HomeScreen(
     navController: NavHostController,
     modifier: Modifier = Modifier
 ) {
+    val context = LocalContext.current
+    val application = context.applicationContext as Application
+    val viewModel = remember {
+        SharedWithViewModel(application, context)
+    }
+    val state by viewModel.sharedWithState.collectAsState()
+    var sharedUsers by remember { mutableStateOf<List<SharedWithResponse>?>(emptyList()) }
+
+    LaunchedEffect(Unit) {
+        viewModel.fetchSharedWith(22)
+    }
+
+    when (state) {
+        is SharedWithState.Loading -> {
+            Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+                CircularProgressIndicator()
+            }
+        }
+        is SharedWithState.Success -> {
+           sharedUsers = (state as SharedWithState.Success).sharedWithResponse
+            Log.e("SharedWithState.Success", "Thành công")
+        }
+        is SharedWithState.Error -> {
+            val errorMessage = (state as SharedWithState.Error).error
+            Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+                Text(text = errorMessage, color = MaterialTheme.colorScheme.error)
+            }
+            Log.e("SharedWithState.Error", "Thất bại")
+        }
+        else -> {}
+    }
+
     AppTheme {
         val colorScheme = MaterialTheme.colorScheme
         Scaffold(containerColor = colorScheme.background,
@@ -170,11 +216,26 @@ fun HomeScreen(
                         }
                     }
 
-                    LazyRow(
-                        modifier = Modifier.fillMaxWidth()
-                    ) {
-                        items(5) {
-                            DeviceCard()
+                    fun getType(typeId: Int): String {
+                        return when (typeId) {
+                            1 -> "Báo cháy" // Fire
+                            2 -> "Đèn led" // Light
+                            else -> "Không xác định"
+                        }
+                    }
+
+                    sharedUsers?.let { response ->
+                        LazyRow(
+                            modifier = Modifier.fillMaxWidth()
+                        ) {
+                            items(response) { device ->
+                                DeviceCard(
+                                    device,
+                                    navController,
+                                    deviceName = device.Device.Name,
+                                    deviceType = getType(device.Device.TypeID),
+                                )
+                            }
                         }
                     }
                 }
