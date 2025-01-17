205d607e4eae1ff68130d7d2c014cf94e3c96a90
Nguyễn Thanh Sang <nguyenthanhsang92643@gmail.com>
2025-01-17 11:52:23 +0700

    Bổ sung DeviceRepository.kt

--- app/src/main/java/com/example/ungdungquanlyvadieukhieniot_homeconnect/data/repository/DeviceRepository.kt
+++ app/src/main/java/com/example/ungdungquanlyvadieukhieniot_homeconnect/data/repository/DeviceRepository.kt
@@ -1,10 +1,13 @@
 package com.example.ungdungquanlyvadieukhieniot_homeconnect.data.repository
 
 import android.content.Context
+import android.util.Log
 import com.example.ungdungquanlyvadieukhieniot_homeconnect.data.remote.api.RetrofitClient
 import com.example.ungdungquanlyvadieukhieniot_homeconnect.data.remote.dto.AttributeRequest
 import com.example.ungdungquanlyvadieukhieniot_homeconnect.data.remote.dto.AttributeResponse
 import com.example.ungdungquanlyvadieukhieniot_homeconnect.data.remote.dto.DeviceResponse
+import com.example.ungdungquanlyvadieukhieniot_homeconnect.data.remote.dto.LinkDeviceRequest
+import com.example.ungdungquanlyvadieukhieniot_homeconnect.data.remote.dto.LinkedDeviceResponse
 import com.example.ungdungquanlyvadieukhieniot_homeconnect.data.remote.dto.ToggleRequest
 import com.example.ungdungquanlyvadieukhieniot_homeconnect.data.remote.dto.ToggleResponse
 import com.example.ungdungquanlyvadieukhieniot_homeconnect.data.remote.dto.UnlinkResponse
@@ -34,5 +37,20 @@ class DeviceRepository (private val context: Context) {
         val sharedPrefs =context.getSharedPreferences("MY_APP_PREFS", Context.MODE_PRIVATE)
         val token=  sharedPrefs.getString("JWT_TOKEN", "") ?: ""
         return apiService.postUnlink(deviceId, token = "Bearer $token")
+    }
+
+    suspend fun linkDevice(deviceId: String, spaceId: String, deviceName: String): LinkedDeviceResponse {
+        val sharedPrefs = context.getSharedPreferences("MY_APP_PREFS", Context.MODE_PRIVATE)
+        val token = sharedPrefs.getString("JWT_TOKEN", "") ?: ""
+
+        Log.d("LinkDeviceRepository", "Token: Bearer $token")
+        Log.d("LinkDeviceRepository", "Payload: deviceId = $deviceId, spaceId = $spaceId, deviceName = $deviceName")
+
+        return apiService.linkDevice(
+            body = LinkDeviceRequest(deviceId, spaceId, deviceName),
+            token = "Bearer $token"
+        ).also {
+            Log.d("LinkDeviceRepository", "API Response: ${it.message}, Device: ${it.device}")
+        }
     }
 }
